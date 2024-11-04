1a1141ca2b2faaa397e7e9d473f2dcef1380c551
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-04 09:12:43 +0300

    task one plp project submission

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("what is your name: ")
+age = input("Enter your age: ")
+location = input("whee are you located: ")
+
+print(f"Hello, {name}! You are {age} years old, and you are currently living in {location}.")
